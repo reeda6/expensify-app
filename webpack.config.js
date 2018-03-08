6609b61005e9815a1028fc72fcf370f18711ac49
reeda6 <alexander.reed@vanderbilt.edu>
2018-03-08 15:26:52 -0500

    Setup Prod webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@
 //__dirname
 
 const path=require('path');
-
-
+const ExtractTextPlugin=require('extract-text-webpack-plugin');
+module.exports=(env)=>{
+console.log(env);
+const isProduction=env==='production';
+const CSSExtract= new ExtractTextPlugin('styles.css');
 console.log(path.join(__dirname,'public'));
-module.exports={
+return{
   entry: './src/app.js',
   output:{
     path:path.join(__dirname,'public'),//abs path
@@ -18,16 +21,36 @@ module.exports={
       exclude: /node_modules/
     },{
       test: /\.s?css$/,
-      use:[
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
+       use:CSSExtract.extract({
+         use:[
+           {
+             loader:'css-loader',
+             options:{
+               sourceMap:true
+             }
+           },
+           {
+             loader:'sass-loader',
+             options:{
+               sourceMap:true
+             }
+           }
+         ]
+       })
+       //[
+      //   'style-loader',
+      //   'css-loader',
+      //   'sass-loader'
+      // ]
     }]
   },
-  devtool:'cheap-module-eval-source-map',
+  plugins:[
+    CSSExtract
+  ],
+  devtool:isProduction?'source-map':'cheap-module-eval-source-map',
   devServer:{
     contentBase:path.join(__dirname,'public'),
     historyApiFallback:true
   }
+};
 };
